f948e33fca7fcccc0b4dd85c443f9425b82441de
Edward Lanto <elanto@hotmail.com>
2020-10-01 16:39:28 -0700

    productionize node server

--- server.js
+++ server.js
@@ -13,7 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(express.static(__dirname + "/fe/public"));
+
+if (DEBUG) {
+  app.use(express.static(__dirname + "/fe/public"));
+} else {
+  app.use(express.static(path.join(__dirname, "/fe/build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/fe/build", "index.html"));
+  });
+}
 
 // Routes
 const authRouter = require("./routes/authRoutes");
@@ -23,4 +32,7 @@ app.use("/", [authRouter]);
 app.use("/spotify", spotifyRouter);
 
 console.log("Listening on 8888");
-app.listen(8888);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
